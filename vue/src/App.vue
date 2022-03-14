1582f17e7ed18c6aaff90c7b3efaff157facb1d7
Ivan Vasilev <i.bozhkov@up2technology.com>
2022-03-14 16:39:00 +0200

    Add Props

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <h2>{{title}}</h2>
     <NavbarTop ></NavbarTop>
+    <AllFriends :friends="friends"></AllFriends>
+    <OnlineFriends :friends="friends"></OnlineFriends>
     <FooterMain />
   </div>   
 </template>
@@ -10,16 +12,28 @@
 
 import FooterMain from './components/FooterMain.vue'
 import NavbarTop from './components/NavbarTop'
+import AllFriends from './components/AllFriends'
+import OnlineFriends from './components/OnlineFriends'
 export default {
   name: 'App',
   data(){
     return{
-      title:'Vue js APP'
+      title:'Vue js APP',
+      friends:[
+            {name:'A', online:true},
+            {name:'B', online:false},
+            {name:'C', online:true},
+            {name:'D', online:true},
+            {name:'E', online:false},
+            {name:'f', online:false}
+        ] 
     }
   },
   components: {
     NavbarTop,
-    FooterMain
+    FooterMain,
+    OnlineFriends,
+    AllFriends
   }
 }
 </script>
