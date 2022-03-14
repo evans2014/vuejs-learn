033cb431637c7ed01d01b3919504270a21631d9c
Ivan Vasilev <i.bozhkov@up2technology.com>
2022-03-14 14:26:50 +0200

    Vue APP

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>First Vuejs</h2>
+    <h1>{{ title }}</h1>   
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      title:'Ttitle vuejs project'
+    }
   }
 }
 </script>
