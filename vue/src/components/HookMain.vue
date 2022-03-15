059e5721d6e736951eaacf94bdca42e5ce5c0483
Ivan Vasilev <i.bozhkov@up2technology.com>
2022-03-15 12:47:08 +0200

    Post data

--- vue/src/components/HookMain.vue
+++ vue/src/components/HookMain.vue
@@ -1,34 +1,39 @@
 <template>
 <div class="Hook">
-  <h1>{{title}}</h1>
-  <button @click="ChangeTitle">Click Me</button>
+  <h1>Posts </h1>
+<div v-for="post in posts" :key="post.id">
+  <h2>{{post.title}}</h2>
+  <p>{{post.body}}</p>
+</div>
 </div>    
 </template>
 <script>
+import axios from 'axios'
 export default {
   name: 'HookMain',
   data() {
     return {
-      title:'Hook Main Data'     
+      posts:[]    
     }
   },
   methods: {
-    ChangeTitle(){
-      this.title='New Title'
-    }
-  },
-  beforeCreate() {
-    alert('Before Create Hook')
-  },
-  created() {
-    alert('Created Hook')
-  },
-  beforeUpdate() {
-    alert('Before update message')
+    
+  },created() {
+    axios.get('https://jsonplaceholder.typicode.com/posts/')
+    .then(response=>{
+      //console.log(response)
+      this.posts = response.data
+    })
+    .catch(error=>{
+      console.log(error)
+    })
   },
 }
 </script>
 <style>
-
+h1{
+  color:red;
+  text-align: center;
+}
 
 </style>
