2299f17bff41acebebe4c0d25bcd405f7424aee2
Ivan Vasilev <i.bozhkov@up2technology.com>
2022-03-15 14:49:24 +0200

    Add Profile Page

---
+++ nvue/router/src/components/Profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="profile">
+    
+    <h2>Profile ({{UserId}})</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  data () {
+    return {
+     UserId:this.$route.params.user_id
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  font-weight: normal;
+}
+
+</style>
